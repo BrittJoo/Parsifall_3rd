d80ce749d4b71bc84dbc08ad5cefecb832396ba6
Unknown <britt-joosten@hotmail.com>
2018-06-20 13:41:46 +0200

    Made the script start

---
+++ Assets/scenes/latest scene/scripts/point section/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Data", menuName = "StatementKeeper", order = 1)]
+public class ScoreKeeper : ScriptableObject
+{
+    public int[][] jaggedArrayStatements = new int[3][];
+    private AcceptChoice acceptChoice;
+
+    void Start()
+    {
+        acceptChoice = FindObjectOfType<AcceptChoice>();
+
+        jaggedArrayStatements[0] = new int[1] { acceptChoice.currentStatement };
+        jaggedArrayStatements[1] = new int[4] { 1, 2, 3, 4 }; ;
+        jaggedArrayStatements[2] = new int[6];
+
+    }
+
+    void Update()
+    {
+
+    }
+}
