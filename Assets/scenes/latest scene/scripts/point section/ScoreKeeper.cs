127e2e66523fe50691d0c5499c2197b4839daf3e
Unknown <britt-joosten@hotmail.com>
2018-06-25 14:10:24 +0200

    Auto stash before merge of "ScriptableObject" and "origin/ScriptableObject"

--- Assets/scenes/latest scene/scripts/point section/ScoreKeeper.cs
+++ Assets/scenes/latest scene/scripts/point section/ScoreKeeper.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "Data", menuName = "StatementKeeper", order = 1)]
 public class ScoreKeeper : ScriptableObject
 {
     public int[][] jaggedArrayStatements = new int[3][];
+    List<int> statementArray = new List<int>();
+    List<int> character = new List<int>();
     private AcceptChoice acceptChoice;
     private int charPoints1;
     private int charPoints2;
@@ -25,6 +28,19 @@ public class ScoreKeeper : ScriptableObject
         jaggedArrayStatements[0] = new int[18] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
         jaggedArrayStatements[1] = new int[4] { 1, 2, 3, 4}; ;
         jaggedArrayStatements[2] = new int[6] { charPoints1, charPoints2, charPoints3, charPoints4, charPoints5, charPoints6 };  ;
+
+        foreach (int o in statementArray) 
+            {
+            for (int i = 0; i < 6; i++)
+            {
+                //character.Adds(i);
+            }
+        }
+
+        foreach (int i in statementArray)
+        {
+
+        }
     }
 
     void Update()
